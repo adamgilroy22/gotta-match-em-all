8420cbe461cd177bc182b4a220b7264c34afd208
Adam Gilroy <adamgilroy22@gmail.com>
2023-01-20 10:09:53 +0000

    Add code to generate the board

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,3 +14,38 @@ const state = {
     totalTime: 0,
     loop: null
 }
+
+const generateGame = () => {
+    const dimensions = selectors.board.getAttribute('data-dimension')
+
+    if (dimensions % 2 !== 0) {
+        throw new Error("The dimension of the board must be an even number.")
+    }
+
+    const pokemon = ['https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/bulbasaur.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/charizard.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/wartortle.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/pikachu.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/mewtwo.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/eevee.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/meowth.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/lapras.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/snorlax.png?raw=true',
+                    'https://github.com/adamgilroy22/gotta-match-em-all/blob/main/assets/images/ditto.png?raw=true']
+    const picks = pickRandom(pokemon, (dimensions * dimensions) / 2) 
+    const items = shuffle([...picks, ...picks])
+    const cards = `
+        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-back"></div>
+                    <div class="card-front"><img src='${item}'></div>
+                </div>
+            `).join('')}
+       </div>
+    `
+    
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
+    selectors.board.replaceWith(parser.querySelector('.board'))
+}
