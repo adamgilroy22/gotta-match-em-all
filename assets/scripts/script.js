6be297b83f567fb1579af47b035b32b3333edc99
Adam Gilroy <adamgilroy22@gmail.com>
2023-01-20 10:01:05 +0000

    Add data-dimension to board

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+const selectors = {
+    gameContainer: document.querySelector('.game-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
+
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
